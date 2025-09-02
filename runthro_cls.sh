1facadc09204da424ce12ce3dd3457b623d2163a
EricwanAR <wanboyao@outlook.com>
2025-09-02 10:59:16 +0800

    Update bash scripts

--- runthro_cls.sh
+++ runthro_cls.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion mlp
-# python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion att
-# python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion diff
-# python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion mlp
-# python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion att
-# python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion diff
-# python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion mlp
-# python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion att
-# python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion diff
-# python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion mlp
-# python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion att
-# python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion diff
+python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion mlp
+python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion att
+python main.py --task cls --loss ce --q-encoder lstm --channels 256 --fusion diff
+python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion mlp
+python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion att
+python main.py --task cls --loss ce --q-encoder mamba --channels 256 --fusion diff
+python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion mlp
+python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion att
+python main.py --task cls --loss ce --q-encoder mha --channels 256 --fusion diff
+python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion mlp
+python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion att
+python main.py --task cls --loss ce --q-encoder gru --channels 256 --fusion diff
 python main.py --task cls --loss ce --q-encoder rn18 --channels 16 --fusion mlp
 python main.py --task cls --loss ce --q-encoder rn18 --channels 16 --fusion att
 python main.py --task cls --loss ce --q-encoder rn18 --channels 16 --fusion diff
--- runthro_reg.sh
+++ runthro_reg.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# python main.py --q-encoder lstm --channels 256 --fusion mlp
-# python main.py --q-encoder lstm --channels 256 --fusion att
-# python main.py --q-encoder lstm --channels 256 --fusion diff
-# python main.py --q-encoder mamba --channels 256 --fusion mlp
-# python main.py --q-encoder mamba --channels 256 --fusion att
-# python main.py --q-encoder mamba --channels 256 --fusion diff
-# python main.py --q-encoder mha --channels 256 --fusion mlp
-# python main.py --q-encoder mha --channels 256 --fusion att
-# python main.py --q-encoder mha --channels 256 --fusion diff
-# python main.py --q-encoder gru --channels 256 --fusion mlp
-# python main.py --q-encoder gru --channels 256 --fusion att
-# python main.py --q-encoder gru --channels 256 --fusion diff
-# python main.py --q-encoder cnn --channels 16 --fusion mlp
-# python main.py --q-encoder cnn --channels 16 --fusion att
-# python main.py --q-encoder cnn --channels 16 --fusion diff
-# python main.py --q-encoder cnn --channels 16 --pcs --fusion mlp
-# python main.py --q-encoder cnn --channels 16 --pcs --fusion att
-# python main.py --q-encoder cnn --channels 16 --pcs --fusion diff
+python main.py --q-encoder lstm --channels 256 --fusion mlp
+python main.py --q-encoder lstm --channels 256 --fusion att
+python main.py --q-encoder lstm --channels 256 --fusion diff
+python main.py --q-encoder mamba --channels 256 --fusion mlp
+python main.py --q-encoder mamba --channels 256 --fusion att
+python main.py --q-encoder mamba --channels 256 --fusion diff
+python main.py --q-encoder mha --channels 256 --fusion mlp
+python main.py --q-encoder mha --channels 256 --fusion att
+python main.py --q-encoder mha --channels 256 --fusion diff
+python main.py --q-encoder gru --channels 256 --fusion mlp
+python main.py --q-encoder gru --channels 256 --fusion att
+python main.py --q-encoder gru --channels 256 --fusion diff
 python main.py --q-encoder rn18 --channels 16 --fusion mlp
 python main.py --q-encoder rn18 --channels 16 --fusion att
 python main.py --q-encoder rn18 --channels 16 --fusion diff
-python main.py --q-encoder rn18 --channels 16 --pcs --fusion mlp
-python main.py --q-encoder rn18 --channels 16 --pcs --fusion att
-python main.py --q-encoder rn18 --channels 16 --pcs --fusion diff
-# python main.py --q-encoder cnn --channels 16 --side-enc lstm --pcs --fusion att
+python main.py--q-encoder rn18 --channels 16 --pcs --fusion diff
+python main.py--q-encoder rn18 --channels 16 --pcs --fusion diff --side-enc mamba
+python main.py--q-encoder rn18 --channels 16 --pcs --fusion diff --side-enc mamba --non-siamese
+python uda.py --q-encoder rn18 --channels 16 --pcs --fusion diff --side-enc mamba --case r2
